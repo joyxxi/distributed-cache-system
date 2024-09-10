dd2a4738f964998d34218418d7ce749892d0eb0a
joyxxi <joyx29580@gmail.com>
2024-09-10 16:33:54 -0700

    Implemented LRU cache algorithm & Unit tests

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/joyxxi/distributed-cache-system
+
+go 1.22.5
---
+++ lru/lru.go
@@ -0,0 +1,82 @@
+package lru
+
+import "container/list"
+
+// LRU cache structure
+type Cache struct {
+	maxBytes int64
+	// bytes already used
+	nbytes int64
+	// doubly linked list used to store values
+	ll *list.List
+	// cache map used to store keys
+	cache map[string]*list.Element
+	// Executed when an entry is purged. Optional.
+	onEvicted func(key string, value Value)
+}
+
+type entry struct {
+	key string
+	value Value
+}
+
+type Value interface {
+	Len() int
+}
+
+// New is the constructor of Cache
+func New(maxBytes int64, onEvicted func(string, Value)) *Cache {
+	return &Cache{
+		maxBytes: maxBytes,
+		ll: list.New(),
+		cache: make(map[string]*list.Element),
+		onEvicted: onEvicted,
+	}
+}
+
+// Get searches for key's value and moves to the front
+func (c *Cache) Get(key string) (value Value, ok bool) {
+	if ele, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*entry)
+		return kv.value, true
+	}
+	return
+}
+
+// RemoveOldest removes the oldest item
+func (c *Cache) RemoveOldest() {
+	ele := c.ll.Back()
+	if ele != nil {
+		c.ll.Remove(ele)
+		kv := ele.Value.(*entry)
+		delete(c.cache, kv.key)
+		c.nbytes -= int64(len(kv.key)) + int64(kv.value.Len())
+		if c.onEvicted != nil {
+			c.onEvicted(kv.key, kv.value)
+		}
+	}
+}
+
+// Add adds a value to the cache
+func (c *Cache) Add(key string, value Value) {
+	// Update value if key already exists. Otherwise, add new value.
+	if ele, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*entry)
+		c.nbytes += int64(value.Len()) - int64(kv.value.Len())
+		kv.value = value
+	} else {
+		ele := c.ll.PushFront(&entry{key, value})
+		c.cache[key] = ele
+		c.nbytes += int64(len(key)) + int64(value.Len())
+	}
+	// Remove old value if current bytes exceeds max bytes.
+	for c.maxBytes != 0 && c.maxBytes < c.nbytes {
+		c.RemoveOldest()
+	}
+}
+
+func (c *Cache) Len() int {
+	return c.ll.Len()
+}
